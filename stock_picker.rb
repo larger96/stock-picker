74bd2537e16e70e22b8922be6270659105813829
Ryan Large <ryan.large.96@googlemail.com>
2020-07-21 17:44:04 +0100

    Improved readability

--- stock_picker.rb
+++ stock_picker.rb
@@ -7,19 +7,19 @@ the best day to buy and the best day to sell. Days start at 0.
 def stock_picker(array)
   profits = {}
   array.each do |stock|
-    buy_idx = array.index(stock)
-    array.each do |next_stock|
-      sell_idx = array.index(next_stock)
-      if buy_idx < sell_idx
-        profits["#{stock}->#{next_stock}"] = next_stock - stock
+    day_of_purchase = array.index(stock)
+    array.each do |sell_value|
+      day_of_sale = array.index(sell_value)
+      if day_of_purchase < day_of_sale
+        profits["#{day_of_purchase}->#{day_of_sale}"] = sell_value - stock
       end
     end
   end
-  max_profit_pair = profits.max_by { |k,v| v }
-  final_result = max_profit_pair[0].split('->')
-  day_to_buy = array.index(final_result[0].to_i)
-  day_to_sell = array.index(final_result[1].to_i)
-  p ary = [day_to_buy, day_to_sell]
+  max_profit = profits.max_by { |k,v| v }
+  best_days = max_profit[0].split('->')
+  day_to_buy = best_days[0].to_i
+  day_to_sell = best_days[1].to_i
+  p [day_to_buy, day_to_sell]
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
